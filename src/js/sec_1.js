b0937517f412d4d609917e73c3919db354659238
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-13 02:49:24 +0300

    small fixes on text animations

--- src/js/sec_1.js
+++ src/js/sec_1.js
@@ -15,21 +15,22 @@ function sec_1() {
         <div id="home" className="row container-fluid">
             <div className="leftDiv   col-sm-6 container " >
                 <div className="content">
-                    <div data-aos="fade-right" className="labelAboveTitle"> Hey, i'm Guy Ezra</div>
-                    <div data-aos="fade-right" className="firstMainHeader">Front End Developer</div>
-                    <div data-aos="fade-right" className="underTitleTxt container ">
+                    <div data-aos="fade-right" data-aos-delay="100" className="labelAboveTitle"> Hey, i'm Guy Ezra</div>
+                    <div data-aos="fade-right" data-aos-delay="200" className="firstMainHeader">Front End Developer</div>
+                    <div data-aos="fade-right" data-aos-delay="300" className="underTitleTxt container ">
                         <p >
                             My biggest passion is to make a website come to life.
                         </p>
                     </div>
-                    <div data-aos="fade-right"  >
+                    <div data-aos="fade-right" data-aos-delay="400"  >
                         <a href="#about" className="aboutMeBtn btn ">ABOUT ME</a>
                         <a href="#contact" className="letsTalk btn ">LET'S CHAT</a>
                     </div>
                 </div >
-                <div className="sideIcons">
-                    <a href="https://github.com/ezraguy" target="_blank" rel="noopener noreferrer"><GitSvg /></a>
-                    <a href="https://www.linkedin.com/in/guyezra" target="_blank" rel="noopener noreferrer"><LinkedInSvg /></a>
+                <div className="sideIcons" >
+                    <a href="https://github.com/ezraguy" target="_blank" rel="noopener noreferrer" ><GitSvg data-aos="fade-left"
+                    /></a>
+                    <a href="https://www.linkedin.com/in/guyezra" target="_blank" rel="noopener noreferrer"><LinkedInSvg data-aos="fade-right" /></a>
 
 
 
