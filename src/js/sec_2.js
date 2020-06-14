3c7f7c044865e2f97d44701dc6579feaf7addf0d
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-14 21:19:04 +0300

    added content to the about section

--- src/js/sec_2.js
+++ src/js/sec_2.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import 'aos/dist/aos.css';
 import '../css/sections.css';
-
+import { ReactComponent as SpeedSvg } from "../svg/speedSvg.svg";
+import { ReactComponent as ResponsiveSvg } from "../svg/responsiveSvg.svg";
+import { ReactComponent as AliveSvg } from "../svg/aliveSvg.svg";
 
 function sec_2() {
 
 
     return (
         <div id="about" className="container-fluid ">
-            <div className="whoMI  container">
+            <div className="whoMI container">
                 <div data-aos="fade-down" className="firstHeader container">WHO IS THIS DUDE?</div>
-                <div data-aos="fade-right" className="textUnderHeader ">Hey, I'm 23 years old front-end developer with a big passion for making websites come to life. My main priority is to  ensure the users have fun on my site while getting the information  they need. It only takes users around 3 seconds  to decide if they want to stay, so in that short time window it's my job to make sure they want to stick around.
+                <div data-aos="fade-right" className="textUnderHeader ">Hey, I'm 23 years old front-end developer with a big passion for making websites come to life. My main priority is to  ensure the users have fun on my site while getting the information  they need. It only takes users around 3 seconds  to decide if they want to stay, so in that short time window it's my job to make sure they want to stick around.</div>
+            </div>
+            <div className="header container">
+                <span className="underLine">My Core Values</span>
 
-        
+            </div>
 
+            <div className="values container-fluid ">
+                <div className="row justify-content-around">
+                    <div className="card col-lg-3 col-md-12  col-sm-12 col-xs-12 "  >
+                        <div className="svgIcon" data-aos="fade-down" data-aos-delay="100"><ResponsiveSvg /></div>
+                        <div className="card-body">
+                            <h3 className="text-bold">RESPONSIVE</h3>
+                            <p>All components will look good no matter the screen size. </p>
+                        </div>
+                    </div>
+                    <div className="card card col-lg-3  col-sm-12 col-xs-12 "  >
+                        <div className="svgIcon" data-aos="fade-down" data-aos-delay="200"><SpeedSvg /></div>
+                        <div className="card-body">
+                            <h3 className="text-bold">FAST</h3>
+                            <p className="card-text">The user's time is valuable So the website has to bee fast and with minimal loading time. </p>
+                        </div>
+                    </div>
+                    <div className="card card col-lg-3  col-sm-12 col-xs-12"  >
+                        <div className="svgIcon" data-aos="fade-down" data-aos-delay="300"><AliveSvg /></div>
+                        <div className="card-body">
+                            <h3 className="text-bold">ALIVE</h3>
+                            <p className="card-text">A website is a living thing. I make sure it looks like it</p>
+                        </div>
+                    </div>
 
                 </div>
 
 
             </div>
-
 
 
         </div>
