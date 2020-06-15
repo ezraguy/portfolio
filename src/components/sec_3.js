1be1017054674800033175c622f48d6f659e9c88
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-15 17:31:33 +0300

    adding a circle component to the skills section

--- src/components/sec_3.js
+++ src/components/sec_3.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/sections.css';
+import SkillCircle from './skillCircle';
 
 
 function sec_3() {
@@ -7,6 +8,12 @@ function sec_3() {
     return (
         <div id="skills" className="container-fluid ">
             <h3>WHAT CAN I DO</h3>
+            <div className="container">
+                <div className="row">
+                    <SkillCircle />
+                </div>
+            </div>
+
 
         </div>
 
