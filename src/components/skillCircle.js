d18d38f4b38410e179e7594341dd512889e914b3
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-15 20:16:18 +0300

    changing files and fixing website description

--- src/components/skillCircle.js
+++ src/components/skillCircle.js
@@ -1,37 +1,26 @@
 import React, { Component } from 'react';
 import '../css/skillCircle.css'
+
+
+
+
 class skillCircle extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            skills: ['html,css,react']
+
+
         }
     }
 
 
 
+
     render() {
         return (
-            <div className="container">
-                <div className="box">
-                    <div className="percent">
-                        <svg>
-                            <circle cx="70" cy="70" r="70"></circle>
-                            <circle cx="70" cy="70" r="70"></circle>
-                        </svg>
-
-                        <div className="number">
-                            <h2>87 <span>%</span></h2>
-                        </div>
-
-                    </div>
-                    <h2 className="text">Progress</h2>
-                </div>
-
-
+            <div id="container">
+                {/* Skill circle will be here */}
             </div>
-
-
         );
 
     }
