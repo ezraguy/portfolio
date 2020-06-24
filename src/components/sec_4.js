fbdfaf43830c5b32dd6c0c3e1f41e1f84406f13a
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-24 18:31:24 +0300

    bug fixes

--- src/components/sec_4.js
+++ src/components/sec_4.js
@@ -12,19 +12,15 @@ function Sec_4() {
         <div id="contact" className="container-fluid">
             {/* A little help for the Netlify bots if you're not using a SSG*/}
 
-            <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-                <input type="text" name="name" />
-                <input type="email" name="email" />
-                <textarea name="message"></textarea>
-            </form>
+
             <h3>Let's Chat </h3>
             <div data-aos="fade-left" className="underLineLight" >&nbsp;</div>
             <div className="container" >
-                <form name="contact" method="post" data-netlify="true">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <input className="mb-4" required placeholder="Name" name="Name" type="text" />
-                    <input className="mb-4" required type="email" placeholder="Email" name="email" />
-                    <textarea required name="message" placeholder="Write your message here..." style={{ height: '200px', resize: 'none' }} ></textarea>
+                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                    <input type="hidden" name="bot-field" />
+                    <input className="mb-4" required placeholder="Name" name="Name" id="name" type="text" />
+                    <input className="mb-4" required type="email" placeholder="Email" name="email" id="email" />
+                    <textarea required name="message" id="message" placeholder="Write your message here..." style={{ height: '200px', resize: 'none' }} ></textarea>
                     <div data-netlify-recaptcha="true" ></div>
                     <button className="submitButton">
                         Submit
