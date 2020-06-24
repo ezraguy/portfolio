3a0cbf2492032964c2f91dd81e81fa24670ef787
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-24 18:52:51 +0300

    fixing name on form

--- src/components/sec_4.js
+++ src/components/sec_4.js
@@ -18,7 +18,7 @@ function Sec_4() {
             <div className="container" >
                 <form name="contact" method="post" >
                     <input type="hidden" name="form-name" value="contact" />
-                    <input className="mb-4" required placeholder="Name" name="Name" id="name" type="text" />
+                    <input className="mb-4" required placeholder="Name" name="name" id="name" type="text" />
                     <input className="mb-4" required type="email" placeholder="Email" name="email" id="email" />
                     <textarea required name="message" id="message" placeholder="Write your message here..." style={{ height: '200px', resize: 'none' }} ></textarea>
                     <button type="submit" className="submitButton">
