c88a469168cc07550517c43ba554d97f540610f5
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-24 18:40:18 +0300

    adding netlify form

--- src/components/sec_4.js
+++ src/components/sec_4.js
@@ -16,13 +16,12 @@ function Sec_4() {
             <h3>Let's Chat </h3>
             <div data-aos="fade-left" className="underLineLight" >&nbsp;</div>
             <div className="container" >
-                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                    <input type="hidden" name="bot-field" />
+                <form name="contact" method="post" >
+                    <input type="hidden" name="form-name" value="contact" />
                     <input className="mb-4" required placeholder="Name" name="Name" id="name" type="text" />
                     <input className="mb-4" required type="email" placeholder="Email" name="email" id="email" />
                     <textarea required name="message" id="message" placeholder="Write your message here..." style={{ height: '200px', resize: 'none' }} ></textarea>
-                    <div data-netlify-recaptcha="true" ></div>
-                    <button className="submitButton">
+                    <button type="submit" className="submitButton">
                         Submit
                     </button>
 
