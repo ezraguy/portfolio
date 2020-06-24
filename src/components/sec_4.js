b527136adeffecf6cd6793cdd92b3a26a9d5dd01
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-24 17:59:33 +0300

    bug fix

--- src/components/sec_4.js
+++ src/components/sec_4.js
@@ -9,8 +9,8 @@ function Sec_4() {
         <div id="contact" className="container-fluid">
             <h3>Let's Chat </h3>
             <div data-aos="fade-left" className="underLineLight" >&nbsp;</div>
-            <div className="container" data-netlify="true">
-                <form action="POST">
+            <div className="container" >
+                <form action="POST" data-netlify="true">
                     <input className="mb-4" required placeholder="Name" name="Name" type="text" />
                     <input className="mb-4" required type="email" placeholder="Email" name="email" />
                     <textarea required name="Message" placeholder="Write your message here..." style={{ height: '200px', resize: 'none' }} ></textarea>
