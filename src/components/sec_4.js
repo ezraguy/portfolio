370939db6058c0c404ab9ad624714499bfd460bc
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-06-24 17:09:29 +0300

    adding contact form

--- src/components/sec_4.js
+++ src/components/sec_4.js
@@ -1,23 +1,16 @@
 
-import React, { useRef } from 'react';
+import React from 'react';
 import '../css/sections.css';
 
 
 function Sec_4() {
-    const nameSpan = useRef();
-    const nameInput = useRef();
-
-
-
-
 
     return (
         <div id="contact" className="container-fluid">
             <h3>Let's Chat </h3>
             <div data-aos="fade-left" className="underLineLight" >&nbsp;</div>
             <div className="container">
-                <form>
-
+                <form className="form-group">
                     <div className="mb-4">
                         <input placeholder="Name" name="name" type="text" />
                     </div>
@@ -26,11 +19,11 @@ function Sec_4() {
                     </div>
                     <textarea name="message" placeholder="Write your message here..." style={{ height: '200px', resize: 'none' }} ></textarea>
 
-                    <div class="button">
+                    <div className="button">
                         <span>Submit</span>
                         <svg>
-                            <polyline class="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
-                            <polyline class="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                            <polyline className="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                            <polyline className="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
                         </svg>
                     </div>
 
