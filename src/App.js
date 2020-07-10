54394c262e5ac4c2142463b12b25c71b1f20db60
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-07-11 02:13:33 +0300

    adding import for aos css and small ui fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Sec2 from './components/sec_2'
 import Sec3 from './components/sec_3'
 import Sec4 from './components/sec_4'
 import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 
 function App() {
   Aos.init();
--- src/components/scrollTopBtn.js
+++ src/components/scrollTopBtn.js
@@ -7,18 +7,18 @@ import $ from 'jquery';
 class ScrollTopBtn extends Component {
     constructor(props) {
         super(props);
-        this.state = { opacity: 0, dis: true }
+        this.state = { visibility: 'hidden', dis: true, opacity: 0 }
     }
 
     componentDidMount() {
 
         window.addEventListener('scroll', () => {
             if (window.pageYOffset > 150) {
-                this.setState({ opacity: 1, dis: false })
+                this.setState({ visibility: 'visible', opacity: 1, dis: false })
 
             }
             else {
-                this.setState({ opacity: 0, dis: true })
+                this.setState({ visibility: 'hidden', opacity: 0, dis: true })
 
             }
         });
@@ -28,7 +28,8 @@ class ScrollTopBtn extends Component {
         $("html, body").animate({ scrollTop: 0 }, 500);
     }
     render() {
-        return (<button id="topBtn" disabled={this.state.dis} style={{ opacity: this.state.opacity }} onClick={() => this.scrollUp()} className="topBtn"><img alt="arrow" src={require('../images/arrow-up.svg')}></img></button>);
+        const { visibility, opacity, dis } = this.state;
+        return (<button id="topBtn" disabled={dis} style={{ visibility: visibility, opacity: opacity }} onClick={() => this.scrollUp()} className="topBtn"><img alt="arrow" src={require('../images/arrow-up.svg')}></img></button>);
     }
 }
 
