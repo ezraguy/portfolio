20d5cc6d4afe83f71b5561d6ff828ee3f70b70a8
ezraguy <55058653+ezraguy@users.noreply.github.com>
2022-02-12 09:44:23 +0200

    small refactoring

--- src/components/about-section.jsx
+++ src/components/about-section.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import 'aos/dist/aos.css';
-import { ReactComponent as SpeedSvg } from '../svg/speedSvg.svg';
-import { ReactComponent as ResponsiveSvg } from '../svg/responsiveSvg.svg';
-import { ReactComponent as AliveSvg } from '../svg/aliveSvg.svg';
+import React from "react";
+import "aos/dist/aos.css";
+import { ReactComponent as SpeedSvg } from "../svg/speedSvg.svg";
+import { ReactComponent as ResponsiveSvg } from "../svg/responsiveSvg.svg";
+import { ReactComponent as AliveSvg } from "../svg/aliveSvg.svg";
 
 function aboutSection() {
   return (
-    <div id="about" className="container-fluid ">
-      <div className="whoMI container">
-        <div className="firstHeader container ">WHO IS THIS DUDE? </div>
-        <div data-aos="fade-left" className="underLineDark"></div>
+    <div id='about' className='container-fluid'>
+      <div className='whoMI container'>
+        <div className='firstHeader container'>WHO IS THIS DUDE?</div>
+        <div data-aos='fade-left' className='underLineDark'></div>
 
-        <div data-aos="fade-right" className="textUnderHeader ">
+        <div data-aos='fade-right' className='textUnderHeader '>
           {`
           Hey, I'm a ${
             new Date().getFullYear() - 1997
@@ -24,41 +24,41 @@ function aboutSection() {
 `}
         </div>
       </div>
-      <div className="header container">
-        <span className="">MY CORE VALUES</span>
-        <div data-aos="fade-right" className="underLineDark"></div>
+      <div className='header container'>
+        <span className='core-values'>MY CORE VALUES</span>
+        <div data-aos='fade-right' className='underLineDark'></div>
       </div>
 
-      <div className="values container-fluid ">
-        <div className="row justify-content-around">
-          <div className="card col-lg-3 col-md-12  col-sm-12 col-xs-12 ">
-            <div className="svgIcon" data-aos="fade-down" data-aos-delay="100">
+      <div className='values container-fluid'>
+        <div className='row justify-content-around'>
+          <div className='card col-lg-3 col-md-12  col-sm-12 col-xs-12'>
+            <div className='svgIcon' data-aos='fade-down' data-aos-delay='100'>
               <ResponsiveSvg />
             </div>
-            <div className="card-body">
-              <h3 className="text-bold">RESPONSIVE</h3>
-              <p>All components will look good no matter the screen size. </p>
+            <div className='card-body'>
+              <h3 className='text-bold'>RESPONSIVE</h3>
+              <p>All components will look good no matter the screen size.</p>
             </div>
           </div>
-          <div className="card card col-lg-3  col-sm-12 col-xs-12 ">
-            <div className="svgIcon" data-aos="fade-down" data-aos-delay="200">
+          <div className='card card col-lg-3  col-sm-12 col-xs-12 '>
+            <div className='svgIcon' data-aos='fade-down' data-aos-delay='200'>
               <SpeedSvg />
             </div>
-            <div className="card-body">
-              <h3 className="text-bold">FAST</h3>
-              <p className="card-text">
-                The user's time is valuable so the website has to be fast and
-                with minimal loading time.{' '}
+            <div className='card-body'>
+              <h3 className='text-bold'>FAST</h3>
+              <p className='card-text'>
+                The user's time is valuable so the website has to be fast and with minimal loading
+                time.
               </p>
             </div>
           </div>
-          <div className="card card col-lg-3  col-sm-12 col-xs-12">
-            <div className="svgIcon" data-aos="fade-down" data-aos-delay="300">
+          <div className='card card col-lg-3  col-sm-12 col-xs-12'>
+            <div className='svgIcon' data-aos='fade-down' data-aos-delay='300'>
               <AliveSvg />
             </div>
-            <div className="card-body">
-              <h3 className="text-bold">ALIVE</h3>
-              <p className="card-text">
+            <div className='card-body'>
+              <h3 className='text-bold'>ALIVE</h3>
+              <p className='card-text'>
                 A website is a living thing. I make sure it looks like it.
               </p>
             </div>
